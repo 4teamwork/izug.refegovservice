1414db55834d980cf7073999a8b9012d39763c1b
Daniel Jowett <d.jowett@4teamwork.ch>
2019-10-29 11:37:29 +0100

    Deprecate marker interfaces used with AT

--- izug/refegovservice/interfaces.py
+++ izug/refegovservice/interfaces.py
@@ -1,3 +1,4 @@
+from zope.deprecation import deprecated
 from zope.interface import Interface
 
 
@@ -7,3 +8,16 @@ class IRefEgovService(Interface):
 
 class IEgovLeistung(Interface):
     """Marker interfaces for refegovservice"""
+
+
+deprecated(
+    ['IEgovLeistung'],
+    'Marker interface has been deprecated in favour of new dexterity schema '
+    'izug.refegovservice.content.e_gov_service.IEGovService'
+    ''
+)
+deprecated(
+    ['IRefEgovService'],
+    'Marker interface has been deprecated in favour of new dexterity schema '
+    'izug.refegovservice.content.ref_e_gov_service.IRefEGovService'
+)
