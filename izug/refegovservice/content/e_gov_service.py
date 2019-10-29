3cc50df45a56d8d73cf8659bbbb89572eb95349e
Daniel Jowett <d.jowett@4teamwork.ch>
2019-10-29 17:10:55 +0100

    Make RichTextWidgets smaller

--- izug/refegovservice/content/e_gov_service.py
+++ izug/refegovservice/content/e_gov_service.py
@@ -1,9 +1,10 @@
 # -*- coding: utf-8 -*-
 from plone.app.textfield import RichText
+from plone.app.textfield.widget import RichTextWidget
+from plone.autoform import directives as form
 from plone.dexterity.content import Item
 from plone.formwidget.contenttree import ObjPathSourceBinder
 from plone.supermodel import model
-from plone.supermodel.directives import fieldset
 from z3c.relationfield import RelationChoice
 from zope.interface import implementer
 
@@ -14,66 +15,73 @@ class IEGovService(model.Schema):
     """ Marker interface and Dexterity Python Schema for EGovService
     """
 
+    form.widget('summary', RichTextWidget, rows=5)
     summary = RichText(
         title=_(u'summary'),
         required=False
     )
 
+    form.widget('generalinformation', RichTextWidget, rows=5)
     generalinformation = RichText(
         title=_(u'generalinformation'),
         required=False
     )
 
+    form.widget('precondition', RichTextWidget, rows=5)
     precondition = RichText(
         title=_(u'precondition'),
         required=False
     )
 
+    form.widget('procedure', RichTextWidget, rows=5)
     procedure = RichText(
         title=_(u'procedure'),
         required=False
     )
 
+    form.widget('forms', RichTextWidget, rows=5)
     forms = RichText(
         title=_(u'forms'),
         required=False
     )
 
+    form.widget('requireddocuments', RichTextWidget, rows=5)
     requireddocuments = RichText(
         title=_(u'requireddocuments'),
         required=False
     )
 
+    form.widget('result', RichTextWidget, rows=5)
     result = RichText(
         title=_(u'result'),
         required=False
     )
 
+    form.widget('cost', RichTextWidget, rows=5)
     cost = RichText(
         title=_(u'cost'),
         required=False
     )
 
+    form.widget('legalbases', RichTextWidget, rows=5)
     legalbases = RichText(
         title=_(u'legalbases'),
         required=False
     )
 
+    form.widget('additionalinformation', RichTextWidget, rows=5)
     additionalinformation = RichText(
         title=_(u'additionalinformation'),
         required=False
     )
 
-    additionalinformation = RichText(
-        title=_(u'additionalinformation'),
-        required=False
-    )
-
+    form.widget('annotations', RichTextWidget, rows=5)
     annotations = RichText(
         title=_(u'annotations'),
         required=False
     )
 
+    form.widget('address', RichTextWidget, rows=4)
     address = RichText(
         title=_(u'address'),
         required=False,
