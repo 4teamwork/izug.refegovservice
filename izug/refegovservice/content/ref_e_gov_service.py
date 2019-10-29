f68c91518634ca3bb273001becdfad797a86b6df
Daniel Jowett <d.jowett@4teamwork.ch>
2019-10-29 18:26:38 +0100

    Just use IBasic for RefEGovService - hide description later

--- izug/refegovservice/content/ref_e_gov_service.py
+++ izug/refegovservice/content/ref_e_gov_service.py
@@ -1,40 +1,23 @@
 # -*- coding: utf-8 -*-
-from plone.autoform import directives
 from plone.dexterity.content import Item
 from plone.supermodel import model
 from plone.formwidget.contenttree import ObjPathSourceBinder
-from z3c.form.interfaces import IAddForm
-from z3c.form.interfaces import IEditForm
 from z3c.relationfield import RelationChoice
-from zope import schema
 from zope.interface import implementer
 
-from plone.app.dexterity import MessageFactory as PLONE_APP_DX_MF
 from izug.refegovservice import _
-
-import six
 
 
 class IRefEGovService(model.Schema):
     """ Marker interface and Dexterity Python Schema for RefEGovService
     """
 
-    # Copy title from IBasic behavior. We don't want description
-    title = schema.TextLine(
-        title=PLONE_APP_DX_MF(u'label_title', default=u'Title'),
-        required=True
-    )
-
-    directives.order_before(title='*')
-
-    directives.omitted('title')
-    directives.no_omit(IEditForm, 'title')
-    directives.no_omit(IAddForm, 'title')
-
     referencedService = RelationChoice(
         title=_(u'referencedService'),
         required=False,
-        source=ObjPathSourceBinder(portal_type=['izug.refegovservice.egovservice'])
+        source=ObjPathSourceBinder(
+            portal_type=['izug.refegovservice.egovservice']
+        )
         # TODO may need to set Widget properties as below
         # widget=ReferenceBrowserWidget(
         #     label='Service Referenz',
@@ -47,12 +30,3 @@ class IRefEGovService(model.Schema):
 class RefEGovService(Item):
     """
     """
-    # copied from metadata.Basic
-    def _get_title(self):
-        return self.context.title
-
-    def _set_title(self, value):
-        if not isinstance(value, six.text_type):
-            raise ValueError('Title must be text.')
-        self.context.title = value
-    title = property(_get_title, _set_title)
