0fae6c438d3b00867f4599a8d61e430e3532ae96
Daniel Jowett <d.jowett@4teamwork.ch>
2019-10-31 15:56:06 +0100

    Add AT to DX upgrade step

---
+++ izug/refegovservice/upgrades/20191031154412_archetypes_to_dexterity/upgrade.py
@@ -0,0 +1,37 @@
+from ftw.upgrade import UpgradeStep
+from ftw.upgrade.migration import InplaceMigrator
+
+
+class ArchetypesToDexterity(UpgradeStep):
+    """Archetypes to dexterity.
+    """
+
+    def __call__(self):
+        self.install_upgrade_profile()
+        self.setup_install_profile('profile-plone.app.relationfield:default')
+        self.migrate_egovservice()
+        self.migrate_refegovservice()
+
+    def migrate_egovservice(self):
+
+        migrator = InplaceMigrator(
+            new_portal_type='izug.refegovservice.egovservice',
+            ignore_fields=('allowDiscussion', 'relatedItems'),
+        )
+
+        for obj in self.objects({'portal_type': 'EgovLeistung'},
+                                'Migrate EgovLeistung to dexterity type '
+                                ' izug.refegovservice.egovservice'):
+            migrator.migrate_object(obj)
+
+    def migrate_refegovservice(self):
+
+        migrator = InplaceMigrator(
+            new_portal_type='izug.refegovservice.refegovservice',
+            ignore_fields=('allowDiscussion', 'relatedItems'),
+        )
+
+        for obj in self.objects({'portal_type': 'RefEgovService'},
+                                'Migrate RefEgovService to dexterity type '
+                                'izug.refegovservice.refegovservice'):
+            migrator.migrate_object(obj)
